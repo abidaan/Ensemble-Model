a819e1ee40fd651c12484e21e605c5d2307954d2
Abidaan <abidaan18@gmail.com>
2015-10-07 07:15:23 -0400

    Added R master program that runs all others programs

---
+++ Ensemble.R
@@ -0,0 +1,10 @@
+#Author: Abidaan Joshua Nagawkar.
+
+#Clear environment variables.
+rm(list=ls())
+
+#Run the project software
+source("Power_Data_Cleaning.R")
+source("Weather_Data_Cleaning.R")
+source("Data_Merge.R")
+source("Modeling_and_Visualizations.R")
